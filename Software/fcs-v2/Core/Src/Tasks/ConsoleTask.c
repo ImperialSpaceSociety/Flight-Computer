a6d39642e3def0a12ba5399e24fcdae173497cd7
Yusen Hu <richardyusen@gmail.com>
2023-07-01 12:51:32 -0400

    Add compile-time switch to choose between printf/USB (staging)

--- Software/fcs-v2/Core/Src/Tasks/ConsoleTask.c
+++ Software/fcs-v2/Core/Src/Tasks/ConsoleTask.c
@@ -6,6 +6,7 @@
 #include "usb_device.h"
 #include "usbd_cdc_if.h"
 
+#include <stdio.h>
 #include <string.h>
 
 
@@ -19,10 +20,31 @@ void ConsoleTask(void const * argument) {
 
     if (e.status == osEventMessage) {
       char *p = e.value.p;
+
+#ifdef USE_USB_CDC
       CDC_Transmit_FS((uint8_t *)p, strnlen(p, DLOG_MAX_MSG_LEN));
+#else
+      printf("%.*s", strnlen(p, DLOG_MAX_MSG_LEN), p);
+#endif
+
       free(p);
     }
 
   }
 
 }
+
+/*
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart?, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
+*/
+
