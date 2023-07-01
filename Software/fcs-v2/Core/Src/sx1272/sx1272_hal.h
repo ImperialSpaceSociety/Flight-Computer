857d92734e51d54102f0e88c4daedd7975ff77c2
Yusen Hu <richardyusen@gmail.com>
2023-07-01 10:24:50 -0400

    Add sx1272 driver code folder

---
+++ Software/fcs-v2/Core/Src/sx1272/sx1272_hal.h
@@ -0,0 +1,44 @@
+
+/*
+    Lightweight hardware abstraction layer for
+    platform dependent functions
+ */
+
+
+#ifndef _sx_hal_h
+#define _sx_hal_h
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#include "stm32f446xx.h"
+
+
+typedef struct sx_gpio {
+
+  // Platform-dependent GPIO struct
+  // i.e. information needed to write to a GPIO
+
+  // On STM32 using HAL, this contains a port and a pin.
+
+  GPIO_TypeDef *port;
+  uint16_t pin;
+
+} sx_gpio_t;
+
+
+
+void sx_gpio_write(sx_gpio_t *gpio, bool value);
+
+int sx_spi_write(uint8_t *buf, uint16_t size, void *spi);
+int sx_spi_read(uint8_t *buf, uint16_t size, void *spi);
+
+void sx_sleep(uint32_t ms);
+
+// Debug logging function
+void sx_dlog(const char *format, ...);
+
+
+
+#endif
---
+++ Software/fcs-v2/Core/Src/sx1272/sx1272_regs.h
@@ -0,0 +1,57 @@
+
+enum sx1272_common_regs {
+
+  SxRegFifo = 0x00,
+  SxRegOpMode,
+
+  SxRegFrfMsb = 0x06,
+  SxRegFrfMib,
+  SxRegFrfLsb,
+  SxRegPaConfig,
+  SxRegPaRamp,
+  SxRegOcp,
+  SxRegLna,
+
+  SxRegDioMapping1 = 0x40,
+  SxRegDioMapping2,
+  SxRegVersion,
+
+  SxRegPaDac = 0x5A,
+};
+
+enum sx1272_regs_lora {
+
+  SxLoraRegFifoAddrPtr = 0x0D,
+  SxLoraRegFifoTxBaseAddr,
+  SxLoraRegFifoRxBaseAddr,
+  SxLoraRegFifoRxCurrentAddr,
+  SxLoraRegIrqFlagsMask,
+  SxLoraRegIrqFlags,
+  SxLoraRegRxNbBytes,
+  SxLoraRegRxHeaderCntValueMsb,
+  SxLoraRegRxHeaderCntValueLsb,
+  SxLoraRegRxPacketCntValueMsb,
+  SxLoraRegRxPacketCntValueLsb,
+  SxLoraRegModemStat,
+  SxLoraRegPktSnrValue,
+  SxLoraRegPktRssiValue,
+  SxLoraRegRssiValue,
+  SxLoraRegHopChannel,
+  SxLoraRegModemConfig1,
+  SxLoraRegModemConfig2,
+  SxLoraRegSymbTimeoutLsb,
+  SxLoraRegPreambleMsb,
+  SxLoraRegPreambleLsb,
+  SxLoraRegPayloadLength,
+  SxLoraRegMaxPayloadLength,
+  SxLoraRegHopPeriod,
+  SxLoraRegFifoRxByteAddr,
+
+  SxLoraRegRssiWideband = 0x2C,
+
+  SxLoraRegDetectOptimize = 0x31,
+
+  SxLoraRegDetectionThreshold = 0x37,
+
+  SxLoraRegSyncWord = 0x39,
+};
